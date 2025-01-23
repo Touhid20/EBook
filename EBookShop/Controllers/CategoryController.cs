c47bed5cb322337783900d46c5442ed96db6aa9a
Admin <Admin@TOUHID>
2025-01-23 23:10:33 +0600

    Retrive Category list

--- EBookShop/Controllers/CategoryController.cs
+++ EBookShop/Controllers/CategoryController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EBookShop.Data;
+using EBookShop.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EBookShop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+
+        {
+            _db = db;
+            
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList =_db.Categories.ToList();
             return View();
         }
     }
