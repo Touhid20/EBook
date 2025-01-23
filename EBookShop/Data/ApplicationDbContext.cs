d9c2f18ea540a5591062a695ab7c41d6382ac53b
Admin <Admin@TOUHID>
2025-01-23 22:38:45 +0600

    Add Cateegory View

---
+++ EBookShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EBookShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EBookShop.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        
+        }
+        public DbSet<Category> Categories { get; set; }
+        
+             
+        
+    }
+}
