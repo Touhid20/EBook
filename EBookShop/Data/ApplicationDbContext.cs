ce2a5a6ce4017d56437a7961d74e1308054a6c69
Admin <Admin@TOUHID>
2025-01-23 22:57:13 +0600

    Insert Data

--- EBookShop/Data/ApplicationDbContext.cs
+++ EBookShop/Data/ApplicationDbContext.cs
@@ -10,8 +10,17 @@ namespace EBookShop.Data
         
         }
         public DbSet<Category> Categories { get; set; }
-        
-             
-        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+
+
+
     }
 }
--- EBookShop/Migrations/20250123165422_InsertdateCategoryTable.Designer.cs
+++ EBookShop/Migrations/20250123165422_InsertdateCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using EBookShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EBookShop.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250123165422_InsertdateCategoryTable")]
+    partial class InsertdateCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
