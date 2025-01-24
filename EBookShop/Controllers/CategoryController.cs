7e6dacda02975c31a236b99ee51c4b4251e97b26
Admin <Admin@TOUHID>
2025-01-25 00:30:12 +0600

    Before adding validation

--- EBookShop/Controllers/CategoryController.cs
+++ EBookShop/Controllers/CategoryController.cs
@@ -23,5 +23,13 @@ namespace EBookShop.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
