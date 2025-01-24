88a20442c3f2319a1a5990ff9108a51dfa0f6a0f
Admin <Admin@TOUHID>
2025-01-24 23:51:39 +0600

    Create Form to Category

--- EBookShop/Controllers/CategoryController.cs
+++ EBookShop/Controllers/CategoryController.cs
@@ -18,5 +18,10 @@ namespace EBookShop.Controllers
             List<Category> objCategoryList =_db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
